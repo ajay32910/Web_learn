8424b5dcd548da5317d5694e179d4eacf654b23b
ajay32910 <a.ajay.hitk@gmail.com>
2024-06-08 17:19:21 +0530

    indexL.js

--- Javascript/indexL.js
+++ Javascript/indexL.js
@@ -1,8 +1,25 @@
 //data types
 //const, let,var
-const id=2024;
-let a="letVariable"
-var b="varVaribale";
-console.log(a);
-console.log(b);
-console.log(id);
+let let1;//by default value undifined
+// const id=2024;
+// let a="letVariable"
+// var b="varVaribale";
+// console.log(a);
+// console.log(b);
+// console.log(id);
+//datatypes, object ,string, null-> special value, undefine
+// console.log(let1);
+// console.log(null);
+// console.log(typeof(null));//null is object type
+// console.log(typeof("string"));
+let nums="124";
+console.log(typeof(nums));
+let num=Number(nums);
+console.log(typeof(num));
+//"33"-> 33, "33aasa"->NaN
+//Boolean(), ""->false, 
+let str1;//null hai, ya undifined ya empty hai then boolean conversion false hota hai
+console.log(Boolean(str1));
+console.log("1"+2+3);
+console.log(1+2+"3");
+
