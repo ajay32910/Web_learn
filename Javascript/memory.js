22c3086e0bfa33e022859b175937365025767838
ajay32910 <a.ajay.hitk@gmail.com>
2024-06-08 18:08:16 +0530

    string

---
+++ Javascript/memory.js
@@ -0,0 +1,19 @@
+//stack->primitive data return copy
+//heap-> non-primirive data->return reference
+//string in javascript
+let str1=new String("javaScript")
+console.log(typeof str1);
+console.log(str1[0]);
+console.log(str1.charAt(0));
+console.log(str1.indexOf('j'));
+let str2="rohan"
+let str3="sohan";
+console.log(`this is name of ${str2.toUpperCase()} and length of str ${str2.length}`);
+let str4=str1.slice(3,7);
+console.log(str4);
+let str5=str1.substr(1,3);
+console.log(str5);
+//split("-"),replace("search","replaceWith");
+
+
+
